e0ea64ba65a117f7a2a2f693c370e079c8a61867
Bharath <bharath.fpl@gmail.com>
2020-07-25 20:47:10 +0530

    Added dislike

--- controllers/user_activities.py
+++ controllers/user_activities.py
@@ -34,6 +34,28 @@ def add_user_likes(user_id):
     return jsonify(data={"message": "Updated user likes."}), 200
 
 
+@user_activity.route("/user_dislike/<objectid:user_id>", methods=["POST"])
+@jwt_required
+def add_user_dislikes(user_id):
+    if request.is_json:
+        news_id = request.json["news_id"]
+        category = request.json["category"]
+    else:
+        news_id = request.form["news_id"]
+        category = request.form["category"]
+    if not news_id:
+        return jsonify(data={"message": "Invalid news id"}), 400
+    news_id = ObjectId(news_id)
+    likes_updated = db["user_activity"].update({"user_id": user_id},
+                                               {"$pull": {
+                                                   "news_ids." + category:
+                                                       news_id
+                                               }})
+    db["news_articles"].update({"_id": news_id}, {"$inc": {"Likes": -1}},
+                               True)
+    return jsonify(data={"message": "Updated user Dislikes."}), 200
+
+
 @user_activity.route("/user_interest/<objectid:user_id>", methods=["POST"])
 @jwt_required
 def add_user_interest(user_id):
